bf01b02c7eca45b2c14a4fe4c7108d4d5f5e26b5
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-10-04 14:34:41 +0900

    finish modal

--- src/modules/title/components/mobileMenuModal/MobileMenuModal.tsx
+++ src/modules/title/components/mobileMenuModal/MobileMenuModal.tsx
@@ -8,7 +8,7 @@ import {IMobileMenuModal} from './MobileMenuModal.types';
 const MobileMenuModal: IDefaultFC<IMobileMenuModal> = ({isVisible, handleCloseMenu}) => {
   const renderMenuItem = useCallback<(menuButtonItem: IHomeHeaderMenuButton, index: number) => React.JSX.Element>(
     (menuButtonItem, index) => (
-      <a key={`${menuButtonItem.title}_${index}`} href={menuButtonItem.route} className={index === 0 ? '' : 'ml-6'}>
+      <a key={`${menuButtonItem.title}_${index}`} href={menuButtonItem.route} className={index === 0 ? '' : 'mt-6'}>
         <TextJABoldMD>{menuButtonItem.title}</TextJABoldMD>
       </a>
     ),
@@ -17,18 +17,23 @@ const MobileMenuModal: IDefaultFC<IMobileMenuModal> = ({isVisible, handleCloseMe
 
   return (
     isVisible && (
-      <div>
-        <div>
+      <div
+        className={
+          'fixed top-0 bottom-0 right-0 left-0 w-[100%] h-[100%] bg-black px-[8.89%] py-[3.75%] flex flex-col'
+        }>
+        <div className="flex flex-row items-center justify-between">
           <Logo size={40} />
           <button>
             <Image src={closeIcon} width={40} height={40} alt="close_icon" onClick={handleCloseMenu} />
           </button>
         </div>
-        {HOME_HEADER_MENU_BUTTONS.map((menuButtonItem, index) => renderMenuItem(menuButtonItem, index))}
-        <div className={'mt-10'}>
-          <ButtonPrimaryLarge minWidth={'100%'} title={'お問い合わせ'} />
-          <div className={'mt-2'}>
-            <ButtonSecondaryLarge minWidth={'100%'} title={'資料ダウンロード'} />
+        <div className="mt-[6.25%]">
+          {HOME_HEADER_MENU_BUTTONS.map((menuButtonItem, index) => renderMenuItem(menuButtonItem, index))}
+          <div className={'mt-10'}>
+            <ButtonPrimaryLarge minWidth={'100%'} title={'お問い合わせ'} />
+            <div className={'mt-2'}>
+              <ButtonSecondaryLarge minWidth={'100%'} title={'資料ダウンロード'} />
+            </div>
           </div>
         </div>
       </div>
