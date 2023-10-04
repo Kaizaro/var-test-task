102437337c1387e202281ba7df825e99e50d2818
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2023-10-04 09:19:45 +0900

    finish design of news page

--- src/modules/news/components/newsCard/NewsCard.tsx
+++ src/modules/news/components/newsCard/NewsCard.tsx
@@ -1,20 +1,31 @@
 import {COLORS, IDefaultFC, TextJABoldSM, TextJABoldXL} from '@/src/shared';
-import Image from 'next/image';
 import React from 'react';
 import {INewsCard} from './NewsCard.types';
 import {NewsCardBadge} from '../newsCardBadge';
 
 const NewsCard: IDefaultFC<INewsCard> = ({newsArticle}) => {
   return (
-    <div>
-      <div className="absolute top-4 right-4">
-        <NewsCardBadge
-          label={newsArticle.badgeTitle}
-          bgColor={newsArticle.badgeBgColor}
-          textColor={newsArticle.badgeTextColor}
-        />
+    <div className="w-[100%]">
+      <div
+        style={{
+          backgroundImage: `url(${newsArticle.image.src})`,
+          // width: '100%',
+          width: '100%',
+          height: 231,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          borderRadius: 16,
+        }}>
+        {/* <Image src={newsArticle.image} width={'32.03%'} height={231} alt="article_image" /> */}
+        <div className="px-4 py-4 items-end flex flex-row justify-end">
+          <NewsCardBadge
+            label={newsArticle.badgeTitle}
+            bgColor={newsArticle.badgeBgColor}
+            textColor={newsArticle.badgeTextColor}
+          />
+        </div>
       </div>
-      <Image src={newsArticle.image} width={'32.03%'} height={231} alt="article_image" />
       <div>
         <div>
           <TextJABoldSM color={COLORS.white}>{newsArticle.date}</TextJABoldSM>
--- src/modules/news/page/NewsPage.tsx
+++ src/modules/news/page/NewsPage.tsx
@@ -1,4 +1,4 @@
-import {IDefaultFC} from '@/src/shared';
+import {ButtonPrimaryLarge, ButtonPrimaryMedium, IDefaultFC} from '@/src/shared';
 import React, {useCallback, useMemo} from 'react';
 import {NewsPageHeader} from '../components/newsPageHeader/NewsPageHeader';
 import {chunk} from 'lodash';
@@ -13,7 +13,7 @@ const NewsPage: IDefaultFC = () => {
     (newsArticleItem, index) => {
       const key = `${newsArticleItem.title}_${index}`;
       return (
-        <div key={key}>
+        <div key={key} className="w-[32.03%]">
           <NewsCard newsArticle={newsArticleItem} />
         </div>
       );
@@ -24,17 +24,26 @@ const NewsPage: IDefaultFC = () => {
   const renderNewsArticleRow = useCallback<(newsArticleRow: INewsArticle[], index: number) => React.JSX.Element>(
     (newsArticleRow, index) => {
       const key = `${index}`;
+      const className =
+        index === 0
+          ? 'flex flex-row w-[100%] justify-between items-start'
+          : 'flex flex-row w-[100%] justify-between items-start mt-10';
       return (
-        <div key={key}>{newsArticleRow.map((newsArticleItem, index) => renderNewsArticle(newsArticleItem, index))}</div>
+        <div className={className} key={key}>
+          {newsArticleRow.map((newsArticleItem, itemIndex) => renderNewsArticle(newsArticleItem, itemIndex))}
+        </div>
       );
     },
     [renderNewsArticle],
   );
 
   return (
-    <div className="min-h-[100%] flex flex-col align-center px-[20.94%] py-[80px] bg-gray-900">
+    <div className="min-h-[100%] flex flex-col align-center px-[16.67%] py-[80px] bg-gray_900">
       <NewsPageHeader />
-      <div>{newsChunk.map((item, index) => renderNewsArticleRow(item, index))}</div>
+      <div className={'py-[80px]'}>{newsChunk.map((item, index) => renderNewsArticleRow(item, index))}</div>
+      <div className="py-[80px] self-center items-center">
+        <ButtonPrimaryMedium>{'一覧を見る'}</ButtonPrimaryMedium>
+      </div>
     </div>
   );
 };
